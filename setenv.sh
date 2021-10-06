0c081150f47703d3e114ef07e4001834b9fc34d8
durgaprasadprathi <49200883+durgaprasadprathi@users.noreply.github.com>
2021-10-06 19:37:53 +0530

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export JDBC_DRIVER="org.postgresql.Driver"
+export JDBC_URL="jdbc:postgresql://128.199.17.156/exec_module"
+export POSTGRES_USERNAME="exec_module"
+export POSTGRES_PASSWORD="exec_module1010#"
+export WORKING_DIR="/home/execution-module/"
+export TERRAFORM_POSTGRES_CONN_STR="postgres://exec_module1:exec_module1010@128.199.17.156/exec_module?sslmode=disable"
